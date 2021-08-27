4ac7d0baae2be195292b6f0020d6a29b7400fca9
Charles Pinheiro <charles_fagundes@hotmail.com>
2021-08-27 13:21:55 -0300

    Tratamento de exceçoes nas funçoes files by type e download zip, implementaçao das docstrings

--- kenzie/image.py
+++ kenzie/image.py
@@ -12,6 +12,13 @@ MAX_CONTENT_LENGTH = environ.get('MAX_CONTENT_LENGTH')
 
 
 def upload_by_form():
+    '''Faz o upload de um arquivo para um subdiretório específico dentro do diretório
+        de uploads.
+    
+    Returns:
+        tupla: Contendo mensagem que tal arquivo foi criado e o status code.
+    
+    '''
 
     files_list = list(request.files)
 
@@ -44,6 +51,11 @@ def upload_by_form():
 
 
 def list_all_files():
+    '''Lista todos os arquivos que foram feitos upload.
+
+    Returns:
+        tupla: Contendo uma lista com o nome de todos os arquivos e o status code.
+    '''
     
     files_list = os.walk(FILES_DIRECTORY)
 
@@ -56,21 +68,33 @@ def list_all_files():
     return jsonify(all_files), 200
 
 
-def files_by_type(type: str):
+def files_by_type(type: str) -> tuple:
+    '''Procura e lista arquivos de determinado tipo de um diretório.
 
-    files_list = os.walk(FILES_DIRECTORY)
+    Args:
+        type (str): Palavra representando o tipo do arquivo. (jpg, png, gif)
 
-    list_types = []
+    Returns:
+        tupla: Contendo uma lista com o nome dos arquivos de determinado tipo e o status code.
+    '''
+    try:
+        files_list = os.listdir(f'{FILES_DIRECTORY}/{type}')
+        return jsonify(files_list)
 
-    for file in list(files_list):
-
-        if file[0].split('/')[-1] == type:
-            list_types.extend(file[-1])
-
-    return jsonify(list_types), 200
+    except FileNotFoundError:
+        return {'message': 'Diretório não encontrado'}, 404
 
 
 def download_file(file_name: str):
+    '''Faz o download de um arquivo específico.
+
+    Args:
+        file_name (str): Palavra representando o nome do arquivo.
+
+    Returns:
+        tupla: Contendo o download do arquivo especificado
+        (feito pela função send_from_directory do flask) e o status code.
+    '''
 
     type_file = file_name.split('.')[-1]
 
@@ -83,11 +107,25 @@ def download_file(file_name: str):
 
 
 def download_file_zip():
+    '''Recebe argumentos via query params e faz o download em zip dos arquivos de
+        um diretório específico.
+
+    Args:
+        file_type (str): String representando o tipo do arquivo.
+        compression_rate (): String representando a taxa de compressão(número) do arquivo.
+
+    Returns:
+        tupla: Contendo o download do arquivo especificado
+        (feito pela função send_from_directory do flask) e o status code.
+    '''
 
     file_type = request.args.get('file_type')
     compression_rate = request.args.get('compression_rate')
 
     path_zip = safe_join(f'{FILES_DIRECTORY}', file_type)
+
+    if not os.path.exists(path_zip):
+        return {'message': 'Diretório não existe.'}, 404
 
     if len(os.listdir(path_zip)) == 0:
         return {'message': 'Diretório vazio.'}, 404
